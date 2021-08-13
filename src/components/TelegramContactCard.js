e197fc54e60c32a81db505209f2c0a2f2b9035a2
big@20scoops <visava.c@20scoops.net>
2021-08-13 15:28:15 +0700

    styling for easier to verify correction

--- src/components/TelegramContactCard.js
+++ src/components/TelegramContactCard.js
@@ -1,13 +1,30 @@
 /** @format **/
 import React from 'react';
 import {Text, Image, View} from 'react-native';
+import {StyleSheet} from 'react-native';
 
 export const TelegramContactCard = ({telegramAvatarUrl, name}) => {
-  console.log('image ==>', telegramAvatarUrl);
+  const {container, image} = styles;
   return (
-    <View>
-      <Image source={telegramAvatarUrl} style={{width: 50, height: 50}} />
+    <View style={container}>
+      <Image style={image} source={telegramAvatarUrl} />
       <Text>{name}</Text>
     </View>
   );
 };
+
+export const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+    borderWidth: 1,
+    borderColor: 'black',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  image: {
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+    marginRight: 10,
+  },
+});
